fe6de78b4d78c1ceed52412f44d2171919b0d453
AntonioSusca <antonio.esse91@gmail.com>
2024-01-16 21:11:00 +0100

    Push

---
+++ Traccia1_susca.php
@@ -0,0 +1,55 @@
+<?php
+
+class Company{
+
+    public $name;
+    public $location;
+    public $tot_employees;
+
+    public function report(){
+        if ($this->tot_employees == 0){
+            echo "L'ufficio $this->name con sede in $this->location non ha dipendenti" . "\n";
+        }else {
+            echo "L'ufficio $this->name con sede in $this->location ed ha ben $this->tot_employees" . "\n";
+        }
+
+    }
+
+    static public $avg_wage = 1500;
+
+
+
+    public function __construct($_name,$_location,$_tot_employees){
+
+        $this->name = $_name;
+        $this->location = $_location;
+        $this->tot_employees = $_tot_employees;
+
+    }
+
+    public function totWage($months = 12){
+        $avg = Company :: $avg_wage * $months * $this->tot_employees;
+        echo  "L'azienda ha speso $avg \n";
+
+
+
+    }
+
+}
+
+$company1 = new Company("Aulab", "Italia",50);
+$company2 = new Company("Amex", "Usa" , 25000);
+$company3 = new Company("Pirelli", "Italia",0);
+$company4 = new Company("Apple", "USA" , 35000);
+$company5 = new Company("Google" , "Francia", 0);
+
+
+$companies = [$company1, $company2, $company3, $company4, $company5];
+
+foreach ($companies as $company){
+    $company-> report();
+    $company-> totWage();
+}
+
+$month = readline("Inserisci mensilità");
+$company1 -> totWage($month);
